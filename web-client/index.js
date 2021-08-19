7815c5068caddac43d62ac4bbb489be338161603
Harshvardhan <singhharsh5000@gmail.com>
2021-08-19 12:28:10 +0530

    Added backend support for pages

--- web-client/index.js
+++ web-client/index.js
@@ -11,6 +11,14 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
+app.get("/upload", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/public/upload.html"));
+})
+
+app.get("/uploads", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/public/uploads.html"));
+})
+
 app.listen(3000, ()=>{
     console.log("Server Started at port 3000");
 })
