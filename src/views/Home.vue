7b1d09f70eb9476da72acc233fa9f9a83497529c
Ayushsunny <ayush100anand@gmail.com>
2021-09-27 00:55:04 +0530

    files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -127,9 +127,9 @@
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/src/jquery.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 export default ({
   name: 'Home'
 })
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -33,9 +33,9 @@
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/src/jquery.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 export default {
   name: "Upload"
 }
