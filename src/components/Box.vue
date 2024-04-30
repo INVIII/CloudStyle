aceaadc8fd0da36d27141bbe4388cee973097348
Ayushsunny <ayush100anand@gmail.com>
2024-04-30 16:15:45 +0530

    delete file time stamp change

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -78,7 +78,7 @@ export default {
           addFiletoStorage(fileData, 'files');
           setTimeout(() => {
             deleteFileFromCloud(storageRef, fileData);
-          }, 24 * 60 * 60 * 1000);
+          },60 * 1000);
         })
       }
       );
